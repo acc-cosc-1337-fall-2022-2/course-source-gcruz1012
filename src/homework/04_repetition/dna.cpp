f9b66cdaf6a4fd6c75df5913d915f8193ea85ade
Guillermo Cruz <gcruz1012@gmail.com>
2022-10-01 22:39:42 -0400

    hw 4 with tests

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,32 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+int factorial(int num)
+{
+    int sum = 1;
+    while (num>1)
+    {
+        sum*= num;
+        num--;
+    }
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+  while(num1!= num2)
+  {
+    if (num1 < num2)
+    {
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+    }
+    if (num1 > num2)
+    {
+        num1 = num1 - num2;
+    }
+  }   
+  return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,6 +1,9 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
+using std::cout;
+using std::cin;
 
 
 /*
@@ -12,5 +15,57 @@ user enters a y or Y.
 */
 int main() 
 {
+	int user_choice;
+	do
+	{
+		cout<<"1-Factorial"<<"\n";
+		cout<<"2-Greatest Common Divisor"<<"\n";
+		cout<<"3-Exit"<<"\n";
+		cin>>user_choice;
+
+		if (user_choice == 1)
+		{
+			int num;
+
+			cout<<"\n"<<"Enter only the number for the factorial:";
+			cin>>num;
+
+			int result = factorial(num);
+
+			cout<<"The factorial of "<<num<<" is "<< result <<"\n";
+			
+		}
+		else if (user_choice == 2)
+		{
+			int num1,num2;
+
+			cout<<"\n"<<"Enter the first number:";
+			cin>>num1;
+
+			cout<<"\n"<<"Enter the second number:";
+			cin>>num2;
+
+			int result = gcd(num1, num2);
+
+			cout<<"The greatest common divisor of "<< num1 << " and "<< num2<<" is "<< result<<"\n";
+
+		}
+		else if (user_choice == 3)
+		{
+			cout<<"Are you sure you want to exist?"<<"\n";
+			cout<<"1-Yes"<<"\n";
+			cout<<"2-No"<<"\n";
+
+			cin>>user_choice;
+
+			if (user_choice == 2)
+			{
+				user_choice = 3;
+			}
+
+		}
+
+	}
+	while(user_choice!=3);
 	return 0;
 }
