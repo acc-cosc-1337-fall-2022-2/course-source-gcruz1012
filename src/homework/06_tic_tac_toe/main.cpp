c66a85124890ae0413f6c805017cf2993fe4c010
Guillermo Cruz <gcruz1012@gmail.com>
2022-11-09 12:56:32 -0500

    hw 6 with tests

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,36 @@
+#include "tic_tac_toe.h"
+
+using std::cin;
+using std::cout;
+using std::string;
+using std::vector;
 
 int main() 
 {
+	string first_player;
+	TicTacToe game;
+	string choice;
+	int position;
+	
+	do
+	{
+		cout<<"Player One do you wanna use X or O?"<<"\n";
+		cin>>first_player;
+		game.start_game(first_player);
+		while(!game.game_over())
+		{
+			cout<<"\n"<<"Which position (1-9) would you like to place your mark: ";
+			cin>>position;
+			
+			game.mark_board(position);
+			game.display_board();
+		}
 
+		
+
+		cout<<"\n"<<"Would you like to play again (Y/N): ";
+		cin>>choice;
+
+	}while(choice == "Y" || choice == "y");
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,73 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+
+class TicTacToe
+{
+private:
+    std::string player;
+    std::vector<std::string>  pegs{9, " "};
+                                    
+                                    
+
+
+    void set_next_player();
+
+    /*
+        No params
+        return false if vector of string pegs has available slot
+        check for a " "(space) in each element otherwise return true
+    */
+    bool check_board_full();
+
+
+    /*
+        No params
+        set all 9 elements to a " "(space)
+    */
+    void clear_board();
+
+public:
+    // no parameters, return check_board_full function return val
+    bool game_over();
+ 
+
+
+
+
+    /* 
+    first_player func arg val must be X or O
+    in func player(priv var)= first_player fun arg
+    call clear_board func
+    */
+    void start_game(std::string first_player);
+
+
+    /*
+        Mark vector w position -1 equal to player(priv var)
+        Call set_next_player priv func
+    */
+
+    void mark_board(int position);
+
+
+    /* const
+        return player(priv var) val
+    */
+    std::string get_player()
+        const
+        {
+            return player;
+        }
+
+    /* const
+        No params
+        iterate vector of string pegs:
+            display a tictactoe board in 3x3 format
+    */
+    void display_board() const;
+
+
+
+};
